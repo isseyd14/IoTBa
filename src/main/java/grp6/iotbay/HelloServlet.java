8ddd0b67ccfeab11bd9abb5b3749292524bd0813
Alian713 <divy1211.dc@gmail.com>
2023-03-26 09:54:52 +1100

    Migrated to jakarta ee

--- src/main/java/grp6/iotbay/HelloServlet.java
+++ src/main/java/grp6/iotbay/HelloServlet.java
@@ -1,8 +1,12 @@
 package grp6.iotbay;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
