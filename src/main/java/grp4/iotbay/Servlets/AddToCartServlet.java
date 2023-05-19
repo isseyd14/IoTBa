c974ddd7cccfd813a9faea25715d6230d0fba1b7
byyro23 <byron.j.lester@student.uts.edu.au>
2023-05-19 18:50:58 +1000

    Added curly brace

--- src/main/java/grp4/iotbay/Servlets/AddToCartServlet.java
+++ src/main/java/grp4/iotbay/Servlets/AddToCartServlet.java
@@ -26,14 +26,16 @@ import java.util.ArrayList;
 
 @WebServlet(name = "AddToCartServlet", urlPatterns = "/add-to-cart")
 public class AddToCartServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-        @Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
 
         try (PrintWriter out = response.getWriter()) {
-//        	out.print("add to cart servlet");
+            //        	out.print("add to cart servlet");
 
             ArrayList<Cart> cartList = new ArrayList<>();
             int id = Integer.parseInt(request.getParameter("id"));
@@ -63,5 +65,6 @@ public class AddToCartServlet extends HttpServlet {
                 }
             }
         }
-	}
+    }
+}
 
