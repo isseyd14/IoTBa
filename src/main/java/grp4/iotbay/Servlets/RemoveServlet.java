b0522163f25779dc8c9903d955a3e86821e6a28f
byro23 <byron.j.lester@student.uts.edu.au>
2023-05-21 19:10:25 +1000

    Remove product fix

--- src/main/java/grp4/iotbay/Servlets/RemoveServlet.java
+++ src/main/java/grp4/iotbay/Servlets/RemoveServlet.java
@@ -38,7 +38,6 @@ public class RemoveServlet extends HttpServlet {
                 request.setAttribute("errorMessage", "Error: No such product");
                 RequestDispatcher rd = request.getRequestDispatcher("remove-product.jsp");
                 rd.forward(request, response);
-                return;
             }
             request.setAttribute("successMessage", "Product successfully removed");
             RequestDispatcher rd = request.getRequestDispatcher("remove-product.jsp");
